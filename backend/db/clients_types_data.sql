52e2e3025569b20f433b803ff4bc0afc706a4ae9
Ivan Bolshakov <idbolshakov@gmail.com>
2016-10-20 23:51:24 +0500

    Добавлены наборы данных для таблиц clients_types, offers, payments_types

---
+++ backend/db/clients_types_data.sql
@@ -0,0 +1,17 @@
+--
+-- данные вспомогательной таблицы `clients_types`
+--
+
+USE insis_test_task;
+
+INSERT 
+
+    INTO 
+
+        `clients_types` (`description`) 
+
+    VALUES
+
+        ('Физическое лицо'),
+        ('Юридическое лицо');
+        
